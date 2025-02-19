121d2744a28e038501eadf696f46e384c5595d5d
cherianb59 <68258191+cherianb59@users.noreply.github.com>
2025-02-19 15:47:37 +1100

    Update code.py

--- Code/code.py
+++ Code/code.py
@@ -64,34 +64,40 @@ keyboard_layout = KeyboardLayoutUS(keyboard)
 displayio.release_displays()
 
 sda, scl = board.GP16, board.GP17  
-i2c = busio.I2C(scl, sda)
-display_bus = displayio.I2CDisplay(i2c, device_address=0x3C)
-display = adafruit_displayio_ssd1306.SSD1306(display_bus, width=128, height=64)
+try:
+    i2c = busio.I2C(scl, sda)
+    display_bus = displayio.I2CDisplay(i2c, device_address=0x3C)
+    display = adafruit_displayio_ssd1306.SSD1306(display_bus, width=128, height=64)
 
-# Make the display context
+    # Make the display context
 
-splash = displayio.Group()
-display.root_group = splash
+    splash = displayio.Group()
+    display.root_group = splash
 
-def draw_rectangle(splash):
-#clear screen and draw rectange 
+    def draw_rectangle(splash):
+    #clear screen and draw rectange 
 
-    color_bitmap = displayio.Bitmap(128, 64, 1)
-    color_palette = displayio.Palette(1)
-    color_palette[0] = 0xFFFFFF  # White
+        color_bitmap = displayio.Bitmap(128, 64, 1)
+        color_palette = displayio.Palette(1)
+        color_palette[0] = 0xFFFFFF  # White
 
-    bg_sprite = displayio.TileGrid(color_bitmap, pixel_shader=color_palette, x=0, y=0)
-    
-    splash.append(bg_sprite)
+        bg_sprite = displayio.TileGrid(color_bitmap, pixel_shader=color_palette, x=0, y=0)
+        
+        splash.append(bg_sprite)
 
-    # Draw a smaller inner rectangle
-    inner_bitmap = displayio.Bitmap(118, 54, 1)
-    inner_palette = displayio.Palette(1)
-    inner_palette[0] = 0x000000  # Black
-    inner_sprite = displayio.TileGrid(inner_bitmap, pixel_shader=inner_palette, x=5, y=5)
-    
-    splash.append(inner_sprite)
+        # Draw a smaller inner rectangle
+        inner_bitmap = displayio.Bitmap(118, 54, 1)
+        inner_palette = displayio.Palette(1)
+        inner_palette[0] = 0x000000  # Black
+        inner_sprite = displayio.TileGrid(inner_bitmap, pixel_shader=inner_palette, x=5, y=5)
+        
+        splash.append(inner_sprite)
 
+except:
+    splash = []
+    def draw_rectangle(splash):
+        pass 
+        
 draw_rectangle(splash)
 
 splash.append(label.Label(terminalio.FONT, text = " Amazing"     , color=0xFFFF00, x=35, y=28))
@@ -207,7 +213,17 @@ def release_sequence(sequence):
                     print("release",item["buttons"])
     consumer_control.release()
     
+jiggle_time = time.monotonic()     
+    
 while True:
+    first_time = time.monotonic()
+
+    if (first_time - jiggle_time) > 120 :    
+        mouse.move (x=1)
+        time.sleep(0.001)
+        mouse.move(x=-1)    
+        jiggle_time = first_time
+
     #handle all encoder events before key presses 
         
     #print(encoder_1_last_position , -encoder_1.position)
